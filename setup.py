a8087281794306562ec149b63674f6ec8a8c9b0d
ezjinfe <felix.f.jin@ericsson.com>
2020-03-27 17:52:06 +0800

    Modified setup.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A easy practice of machine learning algorithm ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sumatrae/legible",
+    url="https://github.com/sumatrae/legibleml",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
